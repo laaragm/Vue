29671fd0a77297c9b89b9044817e48574765a3c3
Lara Moura <laragalvanim@gmail.com>
2020-08-11 18:11:11 -0300

    Section 2

---
+++ Conditionals and Rendering Lists/Conditionals and Lists/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+        show: true,
+        fruits: ['apple', 'orange', 'banana'],
+        people: [
+            {name: 'Lara', age: 20, color:'purple'},
+            {name: 'Harry', age: 26, color:'orange'}
+        ]
+    }
+});
