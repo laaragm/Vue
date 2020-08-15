ff29d3f95e3dad36fe7d247787c577c1efc81337
Lara Moura <laragalvanim@gmail.com>
2020-08-15 17:21:57 -0300

    Section 9

---
+++ Communicating between components/Exercise - Slots and Dynamic Components/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
+                <hr>
+                <p> {{ selectedComponent }} </p>
+                <!-- <app-blue>This is the blue component</app-blue>
+                <app-green>This is the green component</app-green>
+                <app-red>This is the red component</app-red> -->
+                <component :is="selectedComponent">
+                    <p>Default content</p>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: function(){
+            return {
+                selectedComponent: 'appRed'
+            }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Communicating between components/Exercise - Slots and Dynamic Components/src/components/Red.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>Red template</div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid red;
+        background-color: lightcoral;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
