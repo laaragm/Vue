08e46f92681d06227c3d2ed9fd7ad7f6eab78cd5
Lara Moura <laragalvanim@gmail.com>
2020-08-15 11:38:50 -0300

    Section 8

---
+++ Communicating between components/Exercise/src/components/Server/Server.vue
@@ -0,0 +1,28 @@
+<!-- props: PARENT => CHILDREN
+     $emit: CHILD => PARENT
+     event bus: SIBLING => SIBLING -->
+
+<template>
+    <li 
+        class="list-group-item" 
+        style="cursor: pointer"
+        @click="serverSelected">
+            Server #{{ server.id }}
+    </li>
+</template>
+
+<script>
+    import { serverBus } from '../../main';
+
+    export default {
+        props: ['server'],
+        methods: {
+            serverSelected() {
+                serverBus.$emit('serverSelected', this.server);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
